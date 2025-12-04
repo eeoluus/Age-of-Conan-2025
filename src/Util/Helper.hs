90c1c67c7d401335b84085e7e661de3d84a6cfdd
Eero Uusaro <eerouusaro@gmail.com>
2025-12-05 01:27:36 +0200

    convenience partitioning funcitions

--- src/Util/Helper.hs
+++ src/Util/Helper.hs
@@ -1,6 +1,21 @@
 module Util.Helper
-( whatever
+( split
+, chunk
+, equals
 ) where
 
-whatever :: IO ()
-whatever = print "Perhaps a utility function unless I lose interest"
+-- note: doesn't handle trailing whitespace
+split :: Char -> String -> [String]
+split _ [] = []
+split delimiter s = word : split delimiter (drop 1 s') -- drop to discard the delimiter
+    where (word, s') = break (== delimiter) s -- for example, on space: ("Hello,", " world!")
+
+chunk :: String -> Int -> [String] 
+chunk [] _ = []
+chunk s size = fst partition : chunk (snd partition) size 
+    where partition = splitAt size s
+
+-- all equal to each other
+equals :: Eq a => [a] -> Bool
+equals (x:[]) = True
+equals (x:xs) = if x == head xs then equals xs else False 
